7a459f918ac4c4c69deeb847c80028bb3cb88c5c
masashi.g <masashi.g@gmail.com>
2019-07-14 21:18:41 +0900

    support broadcast (1 to many)

---
+++ mediasoup_v3_broadcast.js
@@ -0,0 +1,418 @@
+//
+// mediasoup_sample
+//   https://github.com/mganeko/mediasoup_v3_example
+//   mediasoup_v3_example is provided under MIT license
+//
+//   This sample is using https://github.com/versatica/mediasoup
+//
+//   Thanks To:
+//     - https://lealog.hateblo.jp/entry/2019/03/25/180850
+//     - https://lealog.hateblo.jp/entry/2019/02/25/144511
+//     - https://github.com/leader22/mediasoup-demo-v3-simple/tree/master/server
+//     - https://github.com/mkhahani/mediasoup-sample-app
+//     - https://github.com/daily-co/mediasoup-sandbox
+//
+// install
+//   npm install socket.io
+//   npm install express
+//   npm install socket.io
+//   npm install mediasoup@3
+//   npm install mediasoup-client@3
+//   npm install browserify
+// or
+//   npm install
+//
+// setup
+//   npm run build-client
+//
+// run
+//   npm run broadcast
+
+'use strict';
+
+// --- read options ---
+const fs = require('fs');
+let serverOptions = {
+  hostName: "localhost",
+  listenPort: 3000,
+  useHttps: false
+};
+let sslOptions = {};
+if (serverOptions.useHttps) {
+  sslOptions.key = fs.readFileSync(serverOptions.httpsKeyFile).toString();
+  sslOptions.cert = fs.readFileSync(serverOptions.httpsCertFile).toString();
+}
+
+// --- prepare server ---
+const http = require("http");
+const https = require("https");
+const express = require('express');
+
+const app = express();
+const webPort = serverOptions.listenPort;
+app.use(express.static('public'));
+
+let webServer = null;
+if (serverOptions.useHttps) {
+  // -- https ---
+  webServer = https.createServer(sslOptions, app).listen(webPort, function () {
+    console.log('Web server start. https://' + serverOptions.hostName + ':' + webServer.address().port + '/');
+  });
+}
+else {
+  // --- http ---
+  webServer = http.Server(app).listen(webPort, function () {
+    console.log('Web server start. http://' + serverOptions.hostName + ':' + webServer.address().port + '/');
+  });
+}
+
+// --- file check ---
+function isFileExist(path) {
+  try {
+    fs.accessSync(path, fs.constants.R_OK);
+    //console.log('File Exist path=' + path);
+    return true;
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      //console.log('File NOT Exist path=' + path);
+      return false
+    }
+  }
+
+  console.error('MUST NOT come here');
+  return false;
+}
+
+// --- socket.io server ---
+const io = require('socket.io')(webServer);
+console.log('socket.io server start. port=' + webServer.address().port);
+
+io.on('connection', function (socket) {
+  console.log('client connected. socket id=' + getId(socket) + '  , total clients=' + getClientCount());
+
+  socket.on('disconnect', function () {
+    // close user connection
+    console.log('client disconnected. socket id=' + getId(socket) + '  , total clients=' + getClientCount());
+    cleanUpPeer(socket);
+  });
+  socket.on('error', function (err) {
+    console.error('socket ERROR:', err);
+  });
+  socket.on('connect_error', (err) => {
+    console.error('client connection error', err);
+  });
+
+  socket.on('getRouterRtpCapabilities', (data, callback) => {
+    if (router) {
+      console.log('getRouterRtpCapabilities: ', router.rtpCapabilities);
+      sendResponse(router.rtpCapabilities, callback);
+    }
+    else {
+      sendReject({ text: 'ERROR- router NOT READY' }, callback);
+    }
+  });
+
+  // --- producer ----
+  socket.on('createProducerTransport', async (data, callback) => {
+    console.log('-- createProducerTransport ---');
+    const { transport, params } = await createTransport();
+    producerTransport = transport;
+    producerTransport.observer.on('close', () => {
+      if (producer) {
+        producer.close();
+        producer = null;
+      }
+      producerTransport = null;
+    });
+    //console.log('-- createProducerTransport params:', params);
+    sendResponse(params, callback);
+  });
+
+  socket.on('connectProducerTransport', async (data, callback) => {
+    await producerTransport.connect({ dtlsParameters: data.dtlsParameters });
+    sendResponse({}, callback);
+  });
+
+  socket.on('produce', async (data, callback) => {
+    console.log('-- produce ---');
+    const { kind, rtpParameters } = data;
+    producer = await producerTransport.produce({ kind, rtpParameters });
+    sendResponse({ id: producer.id }, callback);
+
+    // inform clients about new producer
+    console.log('--broadcast newProducer ---');
+    socket.broadcast.emit('newProducer');
+  });
+
+  // --- consumer ----
+  socket.on('createConsumerTransport', async (data, callback) => {
+    console.log('-- createConsumerTransport ---');
+    const { transport, params } = await createTransport();
+    addConsumerTrasport(getId(socket), transport);
+    transport.observer.on('close', () => {
+      const id = getId(socket);
+      let consumer = getConsumer(getId(socket));
+      if (consumer) {
+        consumer.close();
+        removeConsumer(id);
+      }
+      removeConsumerTransport(id);
+    });
+    //console.log('-- createTransport params:', params);
+    sendResponse(params, callback);
+  });
+
+  socket.on('connectConsumerTransport', async (data, callback) => {
+    console.log('-- connectConsumerTransport ---');
+    let transport = getConsumerTrasnport(getId(socket));
+    if (!transport) {
+      console.error('transport NOT EXIST for id=' + getId(socket));
+      return;
+    }
+    await transport.connect({ dtlsParameters: data.dtlsParameters });
+    sendResponse({}, callback);
+  });
+
+  socket.on('consume', async (data, callback) => {
+    console.log('-- consume ---');
+    let transport = getConsumerTrasnport(getId(socket));
+    if (!transport) {
+      console.error('transport NOT EXIST for id=' + getId(socket));
+      return;
+    }
+    const { consumer, params } = await createConsumer(transport, producer, data.rtpCapabilities); // producer must exist before consume
+    //subscribeConsumer = consumer;
+    addConsumer(getId(socket), consumer);
+    consumer.observer.on('close', () => {
+      console.log('consumer closed ---');
+    })
+
+    console.log('-- consumer ready ---');
+    sendResponse(params, callback);
+  });
+
+  socket.on('resume', async (data, callback) => {
+    console.log('-- resume ---');
+    let consumer = getConsumer(getId(socket));
+    if (!consumer) {
+      console.error('consumer NOT EXIST for id=' + getId(socket));
+      return;
+    }
+    await consumer.resume();
+    sendResponse({}, callback);
+  });
+
+  // ---- sendback welcome message with on connected ---
+  const newId = getId(socket);
+  sendback(socket, { type: 'welcome', id: newId });
+
+  // --- send response to client ---
+  function sendResponse(response, callback) {
+    //console.log('sendResponse() callback:', callback);
+    callback(null, response);
+  }
+
+  // --- send error to client ---
+  function sendReject(error, callback) {
+    callback(error.toString(), null);
+  }
+
+  function sendback(socket, message) {
+    socket.emit('message', message);
+  }
+});
+
+function getId(socket) {
+  return socket.id;
+}
+
+function getClientCount() {
+  // WARN: undocumented method to get clients number
+  return io.eio.clientsCount;
+}
+
+function cleanUpPeer(socket) {
+  const id = getId(socket);
+  const consumer = getConsumer(id);
+  if (consumer) {
+    consumer.close();
+    removeConsumer(id);
+  }
+
+  const transport = getConsumerTrasnport(id);
+  if (transport) {
+    transport.close();
+    removeConsumerTransport(id);
+  }
+}
+
+// ========= mediasoup ===========
+const mediasoup = require("mediasoup");
+const mediasoupOptions = {
+  // Worker settings
+  worker: {
+    rtcMinPort: 10000,
+    rtcMaxPort: 10100,
+    logLevel: 'warn',
+    logTags: [
+      'info',
+      'ice',
+      'dtls',
+      'rtp',
+      'srtp',
+      'rtcp',
+      // 'rtx',
+      // 'bwe',
+      // 'score',
+      // 'simulcast',
+      // 'svc'
+    ],
+  },
+  // Router settings
+  router: {
+    mediaCodecs:
+      [
+        {
+          kind: 'audio',
+          mimeType: 'audio/opus',
+          clockRate: 48000,
+          channels: 2
+        },
+        {
+          kind: 'video',
+          mimeType: 'video/VP8',
+          clockRate: 90000,
+          parameters:
+          {
+            'x-google-start-bitrate': 1000
+          }
+        },
+      ]
+  },
+  // WebRtcTransport settings
+  webRtcTransport: {
+    listenIps: [
+      { ip: '127.0.0.1', announcedIp: null }
+    ],
+    enableUdp: true,
+    enableTcp: true,
+    preferUdp: true,
+    maxIncomingBitrate: 1500000,
+    initialAvailableOutgoingBitrate: 1000000,
+  }
+};
+
+let worker = null;
+let router = null;
+let producerTransport = null;
+let producer = null;
+//let consumerTransport = null;
+//let subscribeConsumer = null;
+
+
+async function startWorker() {
+  const mediaCodecs = mediasoupOptions.router.mediaCodecs;
+  worker = await mediasoup.createWorker();
+  router = await worker.createRouter({ mediaCodecs });
+  //producerTransport = await router.createWebRtcTransport(mediasoupOptions.webRtcTransport);
+  console.log('-- mediasoup worker start. --')
+}
+
+startWorker();
+
+//
+// Room {
+//   id,
+//   transports[],
+//   consumers[],
+//   producers[],
+// }
+//
+
+// --- multi-consumers --
+let transports = {};
+let consumers = {};
+
+function getConsumerTrasnport(id) {
+  return transports[id];
+}
+
+function addConsumerTrasport(id, transport) {
+  transports[id] = transport;
+  console.log('consumerTransports count=' + Object.keys(transports).length);
+}
+
+function removeConsumerTransport(id) {
+  delete transports[id];
+  console.log('consumerTransports count=' + Object.keys(transports).length);
+}
+
+function getConsumer(id) {
+  return consumers[id];
+}
+
+function addConsumer(id, consumer) {
+  consumers[id] = consumer;
+  console.log('consumers count=' + Object.keys(consumers).length);
+}
+
+function removeConsumer(id) {
+  delete consumers[id];
+  console.log('consumers count=' + Object.keys(consumers).length);
+}
+
+async function createTransport() {
+  const transport = await router.createWebRtcTransport(mediasoupOptions.webRtcTransport);
+  console.log('-- create transport id=' + transport.id);
+
+  return {
+    transport: transport,
+    params: {
+      id: transport.id,
+      iceParameters: transport.iceParameters,
+      iceCandidates: transport.iceCandidates,
+      dtlsParameters: transport.dtlsParameters
+    }
+  };
+}
+
+async function createConsumer(transport, producer, rtpCapabilities) {
+  let consumer = null;
+  if (!router.canConsume(
+    {
+      producerId: producer.id,
+      rtpCapabilities,
+    })
+  ) {
+    console.error('can not consume');
+    return;
+  }
+
+  //consumer = await producerTransport.consume({ // NG: try use same trasport as producer (for loopback)
+  consumer = await transport.consume({ // OK
+    producerId: producer.id,
+    rtpCapabilities,
+    paused: producer.kind === 'video',
+  }).catch(err => {
+    console.error('consume failed', err);
+    return;
+  });
+
+  //if (consumer.type === 'simulcast') {
+  //  await consumer.setPreferredLayers({ spatialLayer: 2, temporalLayer: 2 });
+  //}
+
+  return {
+    consumer: consumer,
+    params: {
+      producerId: producer.id,
+      id: consumer.id,
+      kind: consumer.kind,
+      rtpParameters: consumer.rtpParameters,
+      type: consumer.type,
+      producerPaused: consumer.producerPaused
+    }
+  };
+}
+
